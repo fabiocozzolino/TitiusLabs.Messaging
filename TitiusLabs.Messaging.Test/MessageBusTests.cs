33dc73540fe9605a33cf6970467be597ffe38046
fabiocozzolino <cozzolino.fabio@gmail.com>
2019-12-21 17:13:58 +0100

    MessageBus implementation

---
+++ TitiusLabs.Messaging.Test/MessageBusTests.cs
@@ -0,0 +1,37 @@
+using System.Threading;
+using NUnit.Framework;
+
+namespace TitiusLabs.Messaging.Test
+{
+    public class MessageBusTests
+    {
+        public class SimpleMessaging : IMessage
+        {
+
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Post_OneSubscriber_Called()
+        {
+            bool done = false;
+            MessageBus.Current.Subscribe((SimpleMessaging sm) =>
+            {
+                done = true;
+            });
+
+            MessageBus.Current.Post(new SimpleMessaging());
+
+            while(!done)
+            {
+                Thread.Sleep(1000);
+            }
+
+            Assert.Pass();
+        }
+    }
+}
---
+++ TitiusLabs.Messaging/IMessageBus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TitiusLabs.Messaging
+{
+    public interface IMessageBus
+    {
+        void Post<TMessage>(TMessage message) where TMessage : IMessage;
+        void Subscribe<TMessage>(Action<TMessage> subscriber) where TMessage : IMessage;
+    }
+}
