9e0d837a9adfefde98ce01f65f1bcb126b4c5c8e
Fabio Cozzolino <1750273+fabiocozzolino@users.noreply.github.com>
2019-12-22 15:24:05 +0100

    New Tet Added

---
+++ TitiusLabs.Messaging.Samples.WPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TitiusLabs.Messaging;
+
+namespace TitiusLabs.Messaging.Samples.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBus.Current.Subscribe((TimeMessage message) =>
+            {
+                label1.Content = message.Timestamp;
+            });
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(1000);
+                    MessageBus.Current.Post(new TimeMessage
+                    {
+                        Timestamp = DateTime.Now.ToString()
+                    });
+                }
+            });
+        }
+
+        public class TimeMessage : IMessage
+        {
+            public string Timestamp { get; set; }
+        }
+    }
+}
--- TitiusLabs.Messaging.Test/MessageBusTests.cs
+++ TitiusLabs.Messaging.Test/MessageBusTests.cs
@@ -6,7 +6,12 @@ namespace TitiusLabs.Messaging.Test
 {
     public class MessageBusTests
     {
-        public class SimpleMessaging : IMessage
+        public class SimpleMessage : IMessage
+        {
+
+        }
+
+        public class AnotherSimpleMessage : IMessage
         {
 
         }
@@ -20,16 +25,16 @@ namespace TitiusLabs.Messaging.Test
         public void Post_OneSubscriber_Called()
         {
             var tcs = new TaskCompletionSource<bool>();
-            MessageBus.Current.Subscribe((SimpleMessaging sm) =>
+            MessageBus.Current.Subscribe((SimpleMessage sm) =>
             {
                 tcs.SetResult(true);
             });
 
-            MessageBus.Current.Post(new SimpleMessaging());
+            MessageBus.Current.Post(new SimpleMessage());
 
             var result = tcs.Task.Result;
 
-            MessageBus.Current.UnSubscribeAll<SimpleMessaging>();
+            MessageBus.Current.UnSubscribeAll<SimpleMessage>();
 
             Assert.IsTrue(result);
         }
@@ -39,24 +44,58 @@ namespace TitiusLabs.Messaging.Test
         {
             var tcs1 = new TaskCompletionSource<bool>();
             var tcs2 = new TaskCompletionSource<bool>();
+            var count = 0;
 
-            MessageBus.Current.Subscribe((SimpleMessaging sm) =>
+            MessageBus.Current.Subscribe((SimpleMessage sm) =>
             {
+                count++;
                 tcs1.SetResult(true);
             });
-            MessageBus.Current.Subscribe((SimpleMessaging sm) =>
+            MessageBus.Current.Subscribe((SimpleMessage sm) =>
             {
+                count++;
                 tcs2.SetResult(true);
             });
 
-            MessageBus.Current.Post(new SimpleMessaging());
+            MessageBus.Current.Post(new SimpleMessage());
 
             var result1 = tcs1.Task.Result;
             var result2 = tcs2.Task.Result;
 
-            MessageBus.Current.UnSubscribeAll<SimpleMessaging>();
+            MessageBus.Current.UnSubscribeAll<SimpleMessage>();
+
+            Assert.IsTrue(result1 && result2 && count == 2);
+        }
+
+        [Test]
+        public void Subscribe_DifferentMessage_CalledOnce()
+        {
+            var tcs1 = new TaskCompletionSource<bool>();
+            var tcs2 = new TaskCompletionSource<bool>();
+            var count1 = 0;
+            var count2 = 0;
+            MessageBus.Current.Subscribe((SimpleMessage sm) =>
+            {
+                count1++;
+                tcs1.SetResult(true);
+            });
+            MessageBus.Current.Subscribe((AnotherSimpleMessage sm) =>
+            {
+                count2++;
+                tcs2.SetResult(true);
+            });
+
+            MessageBus.Current.Post(new SimpleMessage());
+            MessageBus.Current.Post(new AnotherSimpleMessage());
+
+            var result1 = tcs1.Task.Result;
+            var result2 = tcs2.Task.Result;
+
+            MessageBus.Current.UnSubscribeAll<SimpleMessage>();
+            MessageBus.Current.UnSubscribeAll<AnotherSimpleMessage>();
 
             Assert.IsTrue(result1 && result2);
+            Assert.IsTrue(count1 == 1 && count2 == 1);
         }
     }
 }
